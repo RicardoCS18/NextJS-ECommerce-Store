341c26aaddf105ccb4b67b9e29ed3ff19bfe46e6
RicardoCS18 <ricardog.carreras@gmail.com>
2023-11-20 06:24:31 -0500

    added use-cart

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -7,6 +7,8 @@ import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import { MouseEventHandler } from "react";
 
 interface ProductCardProps {
   data: Product
@@ -16,13 +18,22 @@ const ProductCard: React.FC<ProductCardProps> = ({
   data
 }) => {
 
+  const previewModal = usePreviewModal();
   const router = useRouter();
+
   const handleClick = () => {
     router.push(`/product/${data?.id}`)
   }
 
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
+  }
+
   return (
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      {/* {Images and Actions} */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
           src={data?.images?.[0]?.url}
@@ -33,7 +44,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => { }}
+              onClick={() => { onPreview }}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
